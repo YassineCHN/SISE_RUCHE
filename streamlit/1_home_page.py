40b8c1e243f65c2ca8eeed429cb2b5c6e1c9dc02
YassineCHN <yassine.0806@live.fr>
2026-01-08 19:06:38 +0100

    correctif home page

--- streamlit/1_home_page.py
+++ streamlit/1_home_page.py
@@ -6,12 +6,17 @@ st.sidebar.image("./static/Logo3.png")
 st.sidebar.markdown("# Home page 🎈")
 
 with st.container(horizontal=True):
-    cl1, cl2, cl3, cl4 = st.columns(4)
-    with cl1:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.header("RUCHE")  # le passer en markdown
         st.image("./static/Logo3.png", width="content")
+        st.write("le projet RUCHE vise à ...")
+    with col2:
+        st.header("Architecture applicative : ")
+        st.image("./static/architecture.png", width="content")
+
 
 st.divider()
 
 with st.container():
-    st.header("Architecture applicative : ")
-    st.image("./static/architecture.png", width="content")
+    st.header("Moteur de recherche")
