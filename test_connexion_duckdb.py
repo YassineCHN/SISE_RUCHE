0edd9aa750619b011fec792443c9b09b99b2a6eb
YassineCHN <yassine.0806@live.fr>
2026-01-13 00:24:29 +0100

    nettoyage

--- test_connexion_duckdb.py
+++
@@ -1,6 +0,0 @@
-import duckdb
-
-con = duckdb.connect("data/local.duckdb")
-
-print(con.execute("SHOW TABLES").fetchall())
-con.close()
--- test_creation_duckdb.py
+++
@@ -1,5 +0,0 @@
-import duckdb
-
-con = duckdb.connect("data/local.duckdb")
-con.execute("SELECT 1")
-con.close()
